0bfb6872160ce2bd309686223e7b1a12ec1b30cc
Max Wissink <max.wissink@gmail.com>
2025-12-28 01:01:44 +0100

    reapplied filter to theme and minor fixes

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ const http = require('http');
 const { WebSocketServer } = require('ws');
 const path = require('path');
 const fs = require('fs');
+const securityFilter = require('./program/filter');
 
 // Load logic modules
 const handleSocket = require('./program/websocket-main');
@@ -16,17 +17,18 @@ const wss = new WebSocketServer({ server });
 // 1. Serve Injected HTML
 expressApp.get('/', (req, res) => {
     const themePath = path.join(__dirname, '..', 'user', config.theme, 'html', 'index.html');
-    if (fs.existsSync(themePath)) {
-        let html = fs.readFileSync(themePath, 'utf8');
-        const scriptTag = `<script src="/internal/websocket-injection.js"></script>`;
-        res.send(html.replace('</body>', `${scriptTag}</body>`));
+    if (!securityFilter(themePath)) {
+        if (fs.existsSync(themePath)) {
+            let html = fs.readFileSync(themePath, 'utf8');
+            const scriptTag = `<script src="/internal/websocket-injection.js"></script>`;
+            res.send(html.replace('</body>', `${scriptTag}</body>`));
+        } else {
+            res.status(404).send("Theme not found");
+        }
     } else {
-        res.status(404).send("Theme not found");
+        res.status(500).send("Mallicious code detected in theme, be carefull where you get your themes");
     }
 });
-
-const assetsPath = path.join(__dirname, '..', 'user', config.theme);
-expressApp.use(express.static(assetsPath));
 
 // 2. Serve Injection Script
 expressApp.get('/internal/websocket-injection.js', (req, res) => {
@@ -34,7 +36,7 @@ expressApp.get('/internal/websocket-injection.js', (req, res) => {
 });
 
 // 3. Static Assets (CSS/Images)
-expressApp.use(express.static(path.join(__dirname, '..', 'user', config.theme, 'html')));
+expressApp.use(express.static(path.join(__dirname, '..', 'user', config.theme)));
 
 // 4. WebSocket Connection
 wss.on('connection', (ws) => {
