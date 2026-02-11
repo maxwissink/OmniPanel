dc50a2b38c22642d617e7b5a6909d16898dafa2f
Max Wissink <max.wissink@gmail.com>
2026-02-11 20:54:09 +0100

    turned off "ping pong" in console

--- app/program/websocket-main.js
+++ app/program/websocket-main.js
@@ -18,10 +18,10 @@ if (fs.existsSync(eventsFolder)) {
 module.exports = function handleSocket(ws) {
     ws.isAlive = true;
 
-    // When the phone responds to our ping, set isAlive to true
+    // When the client responds to our ping, set isAlive to true
     ws.on('pong', () => {
         ws.isAlive = true;
-        console.log("ping pong")
+        //console.log("ping pong")
     });
 
     ws.on('message', (rawMessage) => {
