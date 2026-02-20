b3e267f1b5543237497d0050656357adf3aac5a8
Max Wissink <max.wissink@gmail.com>
2026-02-20 23:43:52 +0100

    package info + icon for windows

--- app/main.js
+++ app/main.js
@@ -147,7 +147,7 @@ app.whenReady().then(() => {
     const win = new BrowserWindow({
         width: config.width || 600,
         height: config.height || 400,
-        icon: path.join(__dirname, 'resources', 'assets', 'icon.jpg'),
+        icon: path.join(__dirname, 'build', 'icon.png'),
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
