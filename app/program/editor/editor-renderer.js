1dfcfad02759d767b7235695937bbfc9f283a31a
Max Wissink <max.wissink@gmail.com>
2026-02-24 00:32:38 +0100

    displaying the blocks

--- app/program/editor/editor-renderer.js
+++ app/program/editor/editor-renderer.js
@@ -15,7 +15,38 @@ async function GetBlocks() {
         return new Block(item.name, item.type, item.path, item.children);
     });
 
-    console.log(blocks);
+    buildHtmlTree(blocks, blocksconstainer)
+}
 
-    blocksconstainer.innerHTML = blocks;
+// A helper function to build the UI recursively
+function buildHtmlTree(blocksArray, parentElement) {
+    // Create an unordered list for this level
+    const ul = document.createElement('ul');
+    ul.classList.add('block-list');
+
+    blocksArray.forEach(block => {
+        // Create a list item for the block
+        const li = document.createElement('li');
+        li.textContent = block.name; // Set the text
+
+        if (block.type === 'folder') {
+            li.classList.add('block-folder');
+            
+            // 🛑 THE RECURSION MAGIC 🛑
+            // If this block has children, call THIS exact function again 
+            // to build a sub-list inside this <li>
+            if (block.children && block.children.length > 0) {
+                buildHtmlTree(block.children, li);
+            }
+        } else {
+            // If it's just a file, give it a different class for styling
+            li.classList.add('block-file');
+        }
+
+        // Add the finished item to our list
+        ul.appendChild(li);
+    });
+
+    // Attach the whole list to whatever parent was passed in
+    parentElement.appendChild(ul);
 }
