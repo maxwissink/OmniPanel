533aa4addb4b151992992290df2ce5902f9354a5
Max Wissink <max.wissink@gmail.com>
2026-03-10 14:38:15 +0100

    fixed logging in debugger

--- app/program/websocket-main.js
+++ app/program/websocket-main.js
@@ -30,7 +30,14 @@ module.exports = function handleSocket(ws) {
             let displayData = data;
             if (typeof data === 'object' && data !== null) {
                 displayData = Object.entries(data)
-                    .map(([key, val]) => `${key}: ${val}`)
+                    .map(([key, val]) => {
+                        const hasChildren = val !== null && typeof val === 'object' && Object.keys(val).length > 0;
+                        const childString = hasChildren
+                            ? Object.entries(val).map(([k, v]) => `${k} ${v}`).join(' ')
+                            : val;
+
+                        return `${key}: ${childString}`;
+                    })
                     .join(', ');
             }
             console.log(`[WS] Event Received: ${type} -> ${displayData}`);
